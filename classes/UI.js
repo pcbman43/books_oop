fdc82b22e5a1eeee9802ba07ff817f81f180253b
MKougia <kougia.mihkel@khk.ee>
2022-02-12 23:41:02 +0200

    Refactored addBook functionality

--- classes/UI.js
+++ classes/UI.js
@@ -1,34 +1,43 @@
 class UI {
+    // help function to add DOM element
+    addUIElement(name, classname = '', textcontent = '', atributes = {}){
+        // create element
+        const element = document.createElement(name);
+        // add css style class to element
+        if(classname !== ''){
+            element.className = classname
+        }
+        // add text content to element
+        element.appendChild(document.createTextNode(textcontent))
+        // add atributes to element
+        if(Object.keys(atributes).length > 0){
+            for(let key in atributes){
+                element.setAttribute(key, atributes[key])
+            }
+        }
+        return element
+    }
     addBook(book){
         // create <tr> element
-        const tr = document.createElement('tr');
+        const tr = this.addUIElement('tr')
         // td for title, author and isbn
         for(let name in book){
-            // create <td> element
-            let td = document.createElement('td');
-            // create text element
-            let text = document.createTextNode(book[name]);
-            // add text to <td>
-            td.appendChild(text);
+            // create <td> element and add text
+            let td = this.addUIElement('td', '', book[name])
             // add td to tr
-            tr.appendChild(td);
-            console.log(tr)
+            tr.appendChild(td)
         }
         // X link
         // create <td> element
-        let td = document.createElement('td');
+        let td = this.addUIElement('td')
         // create <a> element
-        const link = document.createElement('a');
-        // set href atribute to <a>
-        link.setAttribute('href', '#');
-        // add text content to <a>
-        link.appendChild(document.createTextNode('X'));
-        // add <a> to <li>
-        td.appendChild(link);
+        const link = this.addUIElement('a', '', 'X', {'href':'#'})
+        // add link to td
+        td.appendChild(link)
         // add td to tr
-        tr.appendChild(td);
+        tr.appendChild(td)
         // add tr to tbody
-        const booksList = document.querySelector('#books-list');
-        booksList.appendChild(tr);
+        const booksList = document.querySelector('#books-list')
+        booksList.appendChild(tr)
     }
 }
